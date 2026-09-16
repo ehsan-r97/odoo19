f853b9fe60a5df3ba2c4e4f6cc8bc4d672614562
Ehsan REZAEI <ehsan.r97@users.noreply.github.com>
2026-09-17 01:35:07 +0330

    1

--- start_odoo.sh
+++ start_odoo.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-# Change to project directory
-cd /home/odoo19/odoo19 || exit 1
-
-# Activate virtual environment
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
 source venv/bin/activate
-
-# Ensure PostgreSQL is running
-if ! pg_isready -q; then
-    echo "PostgreSQL is not running. Starting it now..."
-    sudo service postgresql start
-    sleep 2
-fi
-
-# Launch Odoo with console logging (overrides any logfile setting)
-./odoo-bin -c odoo.conf --log-handler=:INFO "$@"
+exec ./odoo-bin -c "$PWD/odoo.conf"
